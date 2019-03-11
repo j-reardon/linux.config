01826bc28fc821d90ec22aabe69639ca31d1738d
Jason Reardon <reardon.jason@gmail.com>
2019-03-10 22:21:41 -0500

    adding all the configs

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# terminate already running bar instances
+killall -q polybar
+
+# wait until the processes have been shut down
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+# launch bars
+polybar top &
+polybar bottom &
